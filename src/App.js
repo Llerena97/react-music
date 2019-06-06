ae6b6a5621906a7a67372ed24b019429bda2cfb0
Felipe Llerena <llerenafelipe7@gmail.com>
2019-06-06 11:02:09 -0500

    useEffect for queryAPIInfo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Form from './components/Form'
 import Song from './components/Song'
 import axios from 'axios';
@@ -12,8 +12,21 @@ function App() {
   const queryAPILyrics = async search => {
     const url = `https://api.lyrics.ovh/v1/${search.artist}/${search.song}`
     const result = await axios(url);
+    setArtist(search.artist)
     setLyrics(result.data.lyrics);
   }
+
+  const queryAPIInfo = async () => {
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
+    const result = await axios(url);
+    setInfo(result.data.artists[0]);
+  }
+
+  useEffect(
+    () => {
+      queryAPIInfo()
+    }, [artist]
+  );
 
   return (
     <Fragment>
